912db84f6e3f4747ff7ac553f09df3352b118e81
Michael <mkgrks@gmail.com>
2018-02-22 19:55:24 +0200

    right before format

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,28 @@ module.exports = (app) => {
   	});
 	});
 
-  app.post('/getAllUsers', (req, res, next) => {
+  app.post('/bookForSelected', (req, res, next) => {
+		// const { user, data: { date, day, group, time }} = req.body;
+		
+		console.log(req.body)
+  	// query('INSERT INTO reservations VALUES (DEFAULT, $1, $2, $3, $4, $5, $6) ', [user.uid, date, day, time, group, JSON.stringify(user)], (err, rows) =>{
+		// 	if (err) {
+		// 		console.error(`${__dirname} says `);
+		// 		return next(err);
+		// 	}
+		//   if (!rows.length) return next(404);
+		  
+		//   console.log(rows);
+		  return res.json(req.body);
+  	// });
+  });
 
-  	query('SELECT * FROM USERS', null, (err, rows) =>{
+  app.post('/fetchDataForSelected', (req, res, next) => {
+		const { date, hour, group } = req.body;
+		console.log('date: ',date);
+		console.log('hour: ',hour);
+		console.log('group: ',group);
+  	query('SELECT * FROM reservations WHERE date=$1 AND hour=$2 AND group_type=$3', [date, hour, group], (err, rows) =>{
 			if (err) {
 				console.error(`${__dirname} says `);
 				return next(err);
